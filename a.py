56d2e4189689a2f9405eeb1d96d6a73fd56f2c55
semerenlex <34449021+semerenlex@users.noreply.github.com>
2023-04-14 07:48:17 +0300

    Add files via upload

---
+++ a.py
@@ -0,0 +1,10 @@
+# Import pandas library
+!pip install pandas
+
+# Read the online file by the URL provides above, and assign it to variable "df"
+other_path = "https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-DA0101EN-SkillsNetwork/labs/Data%20files/auto.csv"
+df = pd.read_csv(other_path, header=None)
+
+# show the first 5 rows using dataframe.head() method
+print("The first 5 rows of the dataframe")
+df.head(5)
